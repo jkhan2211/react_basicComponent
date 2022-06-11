de9257324e9c58f36bb8de4422450bbbd5e9dedd
jkhan2211 <junaidkhandev123@gmail.com>
2022-06-11 01:59:23 -0400

    basic react - in progress

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,38 @@
+import DonateItem from "./components/DonateItem";
+function App() {
+
+  const donateData = [
+    {
+      id: 'e1',
+      title: 'Joe Bloggs',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'Frank Heimer', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Mary Thomson',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'Tony Stark',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's Get Started!</h2>
+      <DonateItem
+      title={donateData[0].title}
+      amount={donateData[0].amount}
+      date={donateData[0].date}
+      ></DonateItem>
+
+    </div>
+  );
+}
+
+export default App;
